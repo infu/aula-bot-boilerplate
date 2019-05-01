74f830f5a7da9fd71ed5b50f6003847ed8f1e501
infu <infu@volos.aula.bg>
2019-05-01 09:53:03 +0300

    fetch

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import Aula from 'aula'
+import FormData from "form-data";
+import fetch from "node-fetch";
 
 const main = async () => {
 	console.log("BOT starting...")
-	const JO = Aula({user:{email: 'aulatest@aula.bg', password:'qweasd123'}})
+	const JO = Aula({user:{email: 'aulatest@aula.bg', password:'qweasd123'}, ref_FormData: FormData, ref_fetch: fetch})
 	await JO.connect()
 	console.log("Connected")
 	console.log("Getting changelog from server...")
